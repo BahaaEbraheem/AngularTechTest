36632cb22433977f7b6bea3e624d41931e9dca27
BahaaEbraheem <bahaa.ebraheem812@gmail.com>
2021-06-25 13:12:36 +0300

    Commit..

---
+++ AngularTechTest/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularTechTest.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+       
+    }
+}
